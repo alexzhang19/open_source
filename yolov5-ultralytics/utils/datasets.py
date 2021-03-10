74a5ad95df8fe1dfb9e789de03fa5e93c6be1832
alexzhang19 <59107026+alexzhang19@users.noreply.github.com>
2021-03-10 22:50:15 +0800

    FB：解决RuntimeError: cuDNN error，处理显存问题。
    pin_memory=False，num_workers=1。

--- yolov5-ultralytics/utils/datasets.py
+++ yolov5-ultralytics/utils/datasets.py
@@ -78,9 +78,9 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
     # Use torch.utils.data.DataLoader() if dataset.properties will update during training else InfiniteDataLoader()
     dataloader = loader(dataset,
                         batch_size=batch_size,
-                        num_workers=nw,
+                        num_workers=1,
                         sampler=sampler,
-                        pin_memory=True,
+                        pin_memory=False,
                         collate_fn=LoadImagesAndLabels.collate_fn4 if quad else LoadImagesAndLabels.collate_fn)
     return dataloader, dataset
 
